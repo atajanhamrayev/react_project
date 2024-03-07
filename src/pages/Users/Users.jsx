57112a4c1405fa5959a17575246d13aa454112c5
Atajan Hamrayew <atajanhamrayew05042001@gmail.com>
2024-03-07 15:07:50 +0500

    Class Component

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,38 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import axios from "axios";
-
 import UsersComponent from "./../../components/UsersComponent";
 
-const Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          props.setUsers(response.data.items);
-        });
-    }
-  };
-  return (
-    <div>
-      <button
-        onClick={getUsers}
-        className="border-[2px] border-solid border-[#7268ad] w-[100px] hover:bg-[#7268ad] hover:text-white"
-      >
-        Get Users
-      </button>
-      {props.users.map((user) => {
-        return (
-          <UsersComponent
-            user={user}
-            key={user.id}
-            follow={props.follow}
-            unfollow={props.unfollow}
-          />
-        );
-      })}
-    </div>
-  );
-};
+export default class UsersC extends Component {
+  constructor(props) {
+    super(props);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
 
-export default Users;
+  render() {
+    return (
+      <div>
+        {this.props.users.map((user) => {
+          return (
+            <UsersComponent
+              user={user}
+              key={user.id}
+              follow={this.props.follow}
+              unfollow={this.props.unfollow}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
